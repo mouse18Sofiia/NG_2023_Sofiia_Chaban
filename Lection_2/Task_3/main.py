384770026047560787b1f5beb4cd25714746dbff
Sofiia Chaban <sonyamouse05@gmail.com>
2023-10-29 22:49:36 +0200

    Added third task of the second lection

--- Lection_2/Task_3/main.py
+++
@@ -1,7 +0,0 @@
-fox = int(input("Enter the number: "))
-print("Number | Divisors")
-print("-" * 15)
-
-for dragon in range(1, fox + 1):
-    divisors = [str(joker) for joker in range(1, dragon + 1) if dragon % joker == 0]
-    print(f"{dragon} | {', '.join(divisors)}")
