55ea62da8020c6607a02af50f793ee5cfbed187d
Sofiia Chaban <sonyamouse05@gmail.com>
2023-10-29 22:44:46 +0200

    Added fourth task of the first lection

--- Lection_2/Task_3/main.py
+++ Lection_2/Task_3/main.py
@@ -1,20 +1,7 @@
-def is_prime(number):
-    if number <= 1:
-        return False
-    for i in range(2, number):
-        if number % i == 0:
-            return False
-    return True
-
-user_input = int(input("Enter the number: "))
-
+fox = int(input("Enter the number: "))
 print("Number | Divisors")
 print("-" * 15)
 
-for i in range(1, user_input + 1):
-    divisors = [str(j) for j in range(1, i + 1) if i % j == 0]
-    print(f"{i} | {', '.join(divisors)}")
-
-print("\nPrime numbers:")
-prime_numbers = [str(i) for i in range(1, user_input + 1) if is_prime(i)]
-print(", ".join(prime_numbers))
+for dragon in range(1, fox + 1):
+    divisors = [str(joker) for joker in range(1, dragon + 1) if dragon % joker == 0]
+    print(f"{dragon} | {', '.join(divisors)}")
--- Lection_2/Task_4/main.py
+++ Lection_2/Task_4/main.py
@@ -1,10 +1,5 @@
 minions = {'a', 'e', 'i', 'o', 'u'}
-ans = set()
-s = input("Your text:")
-for letter in s:
-  if letter in minions:
-       ans.add(letter)
-print(("Your vowels:"))
-for letter in ans:
- print( letter, end = ' ')
-print()
+banana = input("Your text:")
+vowels = set(letter for letter in banana if letter in minions)
+print("Your vowels:")
+print(" ".join(vowels))
