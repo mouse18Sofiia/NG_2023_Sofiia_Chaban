a09cbaa8dd4af66ccc9a203172974ce0025a7a19
Sofiia Chaban <sonyamouse05@gmail.com>
2023-11-04 22:30:44 +0200

    Added fourth task to the first lection

--- Lection_1/Task_4/main.py
+++ Lection_1/Task_4/main.py
@@ -1,6 +1,6 @@
-august = float(input("Enter the number one: "))
-boss = float(input("Enter the number two: "))
-capAm = input('''What action do you want to take?:
+FirstN = float(input("Enter the number one: "))
+SecondN = float(input("Enter the number two: "))
+operation = input('''What action do you want to take?:
     "+" = plus
     "-" = minus
     "*" = multiplication
@@ -8,28 +8,31 @@ capAm = input('''What action do you want to take?:
     "^" = raise to a power
     "sqrt" = square root
 ''')
-duck = 0
-match capAm:
-    case "+":
-        duck = august + boss
-        print("Result :" f"{august} {capAm} {boss} = {duck}")
-    case '-':
-        duck = august - boss
-        print("Result :" f"{august} {capAm} {boss} = {duck}")
-    case '*':
-        duck = august * boss
-        print("Result :" f"{august} {capAm} {boss} = {duck}")
-    case '/':
-        duck = august / boss
-        print("Result :" f"{august} {capAm} {boss} = {duck}")
-    case '^':
-        duck = august ** boss
-        print("Result :" f"{august} {capAm} {boss} = {duck}")
-    case "sqrt":
-        if august < 0:
+
+
+def calculate_result(operation, x, y):
+    if operation == "+":
+        return x + y
+    elif operation == "-":
+        return x - y
+    elif operation == "*":
+        return x * y
+    elif operation == "/":
+        if y == 0:
+            print("Error: Division by zero")
+            return None
+        return x / y
+    elif operation == "^":
+        return x ** y
+    elif operation == "sqrt":
+        if x < 0:
             print("Error: Square root of a negative number is undefined")
-        else:
-            duck = august ** 0.5
-            print(f"Result: Square root of {august} = {duck}")
-    case _:
+            return None
+        return x ** 0.5
+    else:
         print("Invalid operation")
+        return None
+
+result = calculate_result(operation, FirstN, SecondN)
+if result is not None:
+    print(f"Result: {FirstN} {operation} {SecondN} = {result}")
