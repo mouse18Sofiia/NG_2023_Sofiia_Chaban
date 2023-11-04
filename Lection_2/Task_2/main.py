331c2fbfdb7475e1be0f2dffa5bc45d1249e1efc
Sofiia Chaban <sonyamouse05@gmail.com>
2023-11-04 23:46:31 +0200

    Added second task to the second lection

--- Lection_2/Task_2/main.py
+++ Lection_2/Task_2/main.py
@@ -1,6 +1,15 @@
-cat = []
-mouse = input("Enter an item : ")
-for dog in mouse:
-    if dog.isdigit():
-        cat.append(int(dog)) 
-print("Your list is :" , (cat) )
+# We create an empty list to store numbers from the entered characters
+el = []
+
+# We ask the user to enter characters
+user_input = input("Enter an item: ")
+
+# We go through each character in the entered line
+for nn in user_input:
+    # We check whether the symbol is a number
+    if nn.isdigit():
+        # If it is a number, we convert it to an integer and add it to the "el" list
+        el.append(int(nn))
+
+# We display the list of numbers that were entered
+print("Your list is:", el)
