38154220fb94911056ef218498e280b89c432689
Sofiia Chaban <sonyamouse05@gmail.com>
2023-11-26 22:59:05 +0200

    Added second task of the second lection

--- Lection_2/Task_2/main.py
+++ Lection_2/Task_2/main.py
@@ -1,15 +1,6 @@
-# We create an empty list to store numbers from the entered characters
-el = []
-
-# We ask the user to enter characters
+list_of_numbers = []
 user_input = input("Enter an item: ")
-
-# We go through each character in the entered line
-for nn in user_input:
-    # We check whether the symbol is a number
-    if nn.isdigit():
-        # If it is a number, we convert it to an integer and add it to the "el" list
-        el.append(int(nn))
-
-# We display the list of numbers that were entered
-print("Your list is:", el)
+for symbol in user_input:
+    if symbol.isdigit():
+        list_of_numbers.append(int(symbol))
+print("Your list is:", list_of_numbers)
