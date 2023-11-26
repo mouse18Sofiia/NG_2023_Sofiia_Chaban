b8306836c8adf29c9187a4cf0f49d8eba96bc5a6
Sofiia Chaban <sonyamouse05@gmail.com>
2023-11-26 22:52:24 +0200

    Added fourth task of the first lection

--- Lection_1/Task_4/main.py
+++ Lection_1/Task_4/main.py
@@ -1,38 +1,18 @@
+import math
 FirstN = float(input("Enter the number one: "))
 SecondN = float(input("Enter the number two: "))
-operation = input('''What action do you want to take?:
-    "+" = plus
-    "-" = minus
-    "*" = multiplication
-    "/" = division
-    "^" = raise to a power
-    "sqrt" = square root
-''')
-
-
-def calculate_result(operation, x1, y1):
-    if operation == "+":
-        return x1 + y1
-    elif operation == "-":
-        return x1 - y1
-    elif operation == "*":
-        return x1 * y1
-    elif operation == "/":
-        if y1 == 0:
-            print("Error: Division by zero")
-            return None
-        return x1 / y1
-    elif operation == "^":
-        return x1 ** y1
-    elif operation == "sqrt":
-        if x1 < 0:
-            print("Error: Square root of a negative number is undefined")
-            return None
-        return x1 ** 0.5
-    else:
-        print("Invalid operation")
-        return None
-
-result = calculate_result(operation, FirstN, SecondN)
-if result is not None:
-    print(f"Result: {FirstN} {operation} {SecondN} = {result}")
+match input('Choose an action\n/,*,+,-,root,power\n'):
+      case "/":
+            print(FirstN/SecondN)
+      case"*":
+            print(FirstN*SecondN)
+      case"+":
+            print(FirstN+SecondN)
+      case "-":
+            print(FirstN-SecondN)
+      case "power":
+            print(FirstN**SecondN)
+      case "root":
+            print(FirstN**(1/SecondN))
+      case _:
+            print("Values isn't validity")
